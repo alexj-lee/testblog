56d2388802f5f627cc598a84ecb2bb6096c195f1
Alex Lee <alex.j.lee03@gmail.com>
2023-05-19 15:16:33 -0700

    change banner og image

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -4,7 +4,7 @@ export const GH_USER_REPO = 'sw-yx/swyxkit'; // used for pulling github issues a
 export const SITE_TITLE = 'alexblog';
 export const SITE_DESCRIPTION = "a blog of alex";
 export const DEFAULT_OG_IMAGE =
-	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
+	'bannerpage.png';
 export const MY_TWITTER_HANDLE = 'eel__xela';
 export const MY_YOUTUBE = 'https://youtube.com/swyxTV';
 export const POST_CATEGORIES = ['Paper', 'Note']; // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
